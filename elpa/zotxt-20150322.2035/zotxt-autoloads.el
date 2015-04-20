4eac92602a553d6023a06b3f79a4737103c89a08
phd <bwtian@gmail.com>
2015-04-20 17:29:27 +0800

    + ‘e02_core.el’ + ‘e03_colors.el’ + ‘e04_minibuffer.el’ + ‘e05_basicmodes.el’ + ‘e06_autocomplete.el’ + ‘e07_Abbreviation.el’ + ‘e08_folding.el’ + ‘e10_orgCore.el’ + ‘e11_orgSkeleton.el’ + ‘e12_orgBabel.el’ ...

---
+++ elpa/zotxt-20150322.2035/zotxt-autoloads.el
@@ -0,0 +1,53 @@
+;;; zotxt-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "org-zotxt" "../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/org-zotxt.el"
+;;;;;;  "a2d9d2d37145f39674951edd036f5ad0")
+;;; Generated autoloads from ../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/org-zotxt.el
+
+(autoload 'org-zotxt-mode "org-zotxt" "\
+Toggle org-zotxt-mode.
+With no argument, this command toggles the mode.
+Non-null prefix argument turns on the mode.
+Null prefix argument turns off the mode.
+
+This is a minor mode for managing your citations with Zotero in a
+org-mode document.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "zotxt" "../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/zotxt.el"
+;;;;;;  "ed1680c2e79cf495c2d68e58498da7a7")
+;;; Generated autoloads from ../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/zotxt.el
+
+(autoload 'zotxt-easykey-mode "zotxt" "\
+Toggle zotxt-easykey-mode.
+With no argument, this command toggles the mode.
+Non-null prefix argument turns on the mode.
+Null prefix argument turns off the mode.
+
+This is a minor mode for managing your easykey citations,
+including completion.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/org-zotxt.el"
+;;;;;;  "../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/zotxt-autoloads.el"
+;;;;;;  "../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/zotxt-pkg.el"
+;;;;;;  "../../../../../Users/phd/.emacs.d/elpa/zotxt-20150322.2035/zotxt.el")
+;;;;;;  (21812 50884 231000 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; zotxt-autoloads.el ends here
